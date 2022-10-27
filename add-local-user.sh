ddceab6212e154699c2e253badb97baf644acff4
ryrova@gmail.com <valeraryan@gmail.com>
2022-10-27 11:58:38 -0700

    Added code to check if user is running script with root privileges

--- add-local-user.sh
+++ add-local-user.sh
@@ -3,7 +3,13 @@
 #Enforces that it be executed with superuser (root) privileges.  If the script is not executed with superuser privileges
 #it will not attempt to create a user and returns an exit status of 1.
 
-
+if [[ $(id -u) -ne 0 ]]
+then
+  echo "Not admin. Please log in with root privileges"
+  exit 1
+else
+  echo "Welcome Admin"
+fi
 
 #Prompts the person who executed the script to enter the username (login), the name for person who will be using the
 #account, and the initial password for the account.
