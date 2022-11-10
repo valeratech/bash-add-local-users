a62db892e3e2d7564797f55bfb5ca0d016fac263
ryrova@gmail.com <valeraryan@gmail.com>
2022-11-10 15:09:53 -0800

    Changed the script to take positional arguments instead of read prompts. Added a function for usage

--- add-local-user.sh
+++ add-local-user.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Create a log function to log to syslog/journald upon account creation success or failure
+usage()
+{
+  echo "USAGE: ${0} [USERNAME] [COMMENT]"
+  echo "EXAMPLE:  ${0} Optimus 'Optimus Prime'"
+  exit 1
+}
+
 #Enforces that it be executed with superuser (root) privileges.  If the script is not executed with superuser privileges
 #it will not attempt to create a user and returns an exit status of 1.
 if [[ $(id -u) -ne 0 ]]
@@ -10,14 +18,24 @@ else
   echo "Welcome Admin"
 fi
 
-#Prompts the person who executed the script to enter the username (login), the name for person who will be using the -
-#account, and the initial password for the account.
-read -p "Username: " USER
-read -p "Full Name or Description: " COMMENT
-read -p "Password: " PASSWORD
+#If no arguments pass display a "usage" message
 
-echo "Username is: ${USER}"
-echo "Password is: xxxxxxx"
+if [[ -z "${@}" ]]
+then
+    usage
+fi
+
+#Uses positional arguments to enter the username (login)
+# And a Full name or description for the comments
+USER="${1}"
+COMMENT="${2}"
+
+
+#Randomly generate a password using /dev/urandom
+#Remove specific characters using the -d option and the -c option to complement the characters in SET1.
+#All characters not in the specified set using -dc (in this case, everything except digits) are removed.
+#PASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%&' | head -c 10; echo '') 1> /dev/null
+PASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%&' | head -c 8); echo ' ' 1> /dev/null
 
 #Creates a new user on the local system with the input provided by the user.
 useradd -m -c "${COMMENT}" "${USER}"
@@ -30,7 +48,7 @@ then
   exit 1
 fi
 
-#Create a password for the user from the input provided
+#Create a password for the user from the input provided (Ubuntu)
 echo "${USER}:${PASSWORD}" | chpasswd
 
 #Informs the user if the password was not able to be created for some reason.
@@ -42,8 +60,9 @@ then
 fi
 
 #Force password change on first login.
-password -e "${USER}"
-
+passwd -e "${USER}"
+echo "${PASSWORD}"
+echo "${0}"
 #Return message of user that was successful added and the HOSTNAME
 
 #Displays the username, password, and host where the account was created.  This way the help desk staff can copy the
